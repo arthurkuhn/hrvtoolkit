997e5cb112e63a30ccd4eb57812d24e8b082ae5c
Arthur <arthurkuhn@outlook.com>
2017-10-12 15:41:02 -0400

    Fixes to Tests:
    Former-commit-id: 981c2459192332e699d48c9d4b8439d9b4526686

--- matlab/Comparator/TestSuite.m
+++ matlab/Comparator/TestSuite.m
@@ -1,22 +1,24 @@
 close all;
 fs = 1000;
 
+index = 1:60000;
+
 ecgFile = "G002ecg.mat";
 load(ecgFile);
-sig = G002ecg;
+sig = G002ecg(index);
 runAllTests(sig, fs, ecgFile);
 
 ecgFile = "A1ecg.mat";
 load(ecgFile);
-sig = A1ecg;
+sig = A1ecg(index);
 runAllTests(sig, fs, ecgFile);
 
 ecgFile = "a5c3ecg.mat";
 load(ecgFile);
-sig = a5c3ecg;
+sig = a5c3ecg(index);
 runAllTests(sig, fs, ecgFile);
 
 ecgFile = "a2f1ecg.mat";
 load(ecgFile);
-sig = a2f1ecg;
+sig = a2f1ecg(index);
 runAllTests(sig, fs, ecgFile);
--- matlab/Comparator/runAllTests.m
+++ matlab/Comparator/runAllTests.m
@@ -5,10 +5,8 @@ function runAllTests( sig, fs, ecgFile)
 [~,locs_Rwave] = findpeaks(sig,'MinPeakHeight',0.0005,'MinPeakDistance',200);   
 printHRV(sig, locs_Rwave, fs, "Unfiltered Signal - " + ecgFile);
 
-
 [~,qrs_i_raw,~] = pan_tompkin(sig,fs,0); % Set last param to 1 for plots
 printHRV(sig, qrs_i_raw, fs, "Pan & Tompkins Algorithm - " + ecgFile);
-
 
 [peaks] = kotaFunction(sig, fs);
 printHRV(sig, peaks, fs, "Base Kota - " + ecgFile);
