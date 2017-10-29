3d39c36e6b58561c0977c00d25237b534c98b964
Kumbidou <claradionet@gmail.com>
2017-10-29 19:12:29 -0400

    ECG vs HR
    Former-commit-id: f5edc4690be28b944e4b03160bd051a18297bffc

--- matlab/Anomaly Detection/ECG_vs_HR.m
+++ matlab/Anomaly Detection/ECG_vs_HR.m
@@ -17,14 +17,19 @@ filt_sig = filter(filt, orig_sig); % new signal
 time = 0:(1/fs):((length(filt_sig)-1)/fs); %time in seconds
 window = 1*fs; % # of secs
 %threshold = 0.09;
-
+% 0.09 for G002, 0.11 for a2f1, 0.145 for a5c3
 [array_ecg,noisy_sig_ecg, s] = std_dev(filt_sig, 0.09, window);
 [qrs_amp_raw,qrs_i_raw,delay]=pan_tompkin(orig_sig,1000,0);
 period = diff(qrs_i_raw);
 period(length(period)+1) = 0;
 period = period./fs; %period in seconds
 [array_pt,noisy_sig_pt,spt] = std_dev(period, 0.04, 8);
-[qrs_amp_raw,qrs_i_raw]=kotaFunction2(orig_sig,1000,0);
+
+[qrs_amp_raw2,qrs_i_raw2]=kotaFunction2(orig_sig,1000,0);
+period2 = diff(qrs_i_raw2);
+period2(length(period2)+1) = 0;
+period2 = period2./fs; %period in seconds
+[array_kt,noisy_sig_kt,skt] = std_dev(period2, 0.04, 8);
 
 
 % array_pt = qrs_i_raw(array_pt);
@@ -70,25 +75,56 @@ xlabel('Time (s)');
 
 linkaxes([ax1, ax2, ax3], 'x')
 
+% Pan and tompkins 
 figure;
-subplot(2,1,1);
+bx1 = subplot(4,1,1);
 plot(1:length(period), period);
-%title('HR');
+title('Pan and Tompkins RR interval (s)');
 ylabel('RR interval (s)');
+axis([0 700 0 1]);
 
-subplot(2,1,2);
+bx2 = subplot(4,1,2);
 plot(1:length(period), period);
 hold on
 %plot(array_pt, noisy_sig_pt, 'rv');
 scatter(array_pt, noisy_sig_pt,15, 'r');
-%title('noisy HR');
+title('Noise detection on Pan and Tompkins (s)');
 ylabel('RR interval (s)');
+axis([0 700 0 1]);
+
+bx3 = subplot(4,1,3);
+plot(1:length(period2), period2);
+title('Kota RR interval (s)');
+ylabel('RR interval (s)');
+axis([0 700 0 1]);
+
+bx4 = subplot(4,1,4);
+plot(1:length(period2), period2);
+hold on
+%plot(array_pt, noisy_sig_pt, 'rv');
+scatter(array_kt, noisy_sig_kt,15, 'r');
+title('Noise detection on Kota');
+ylabel('RR interval (s)');
+axis([0 700 0 1]);
+
+linkaxes([bx1, bx2, bx3, bx4], 'x')
 
 figure
-subplot(2,1,1);
+subplot(4,1,1);
 plot(1:length(period), period);
-title('RR period');
-subplot(2,1,2);
+title('Pan and Tompkins RR interval (s)');
+axis([0 700 0 1]);
+subplot(4,1,2);
 plot(1:length(period), spt)
-title('standard deviation of HR');
+title('Standard deviation of PT RR interval');
+axis([0 700 0 0.3]);
+
+subplot(4,1,3);
+plot(1:length(period2), period2);
+title('Kota RR interval (s)');
+axis([0 700 0 1]);
+subplot(4,1,4);
+plot(1:length(period2), skt)
+title('Standard deviation of Kota RR interval');
+axis([0 700 0 0.3]);
 
