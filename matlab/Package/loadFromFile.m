8592adf1a6f425b8f516a4e2e5adff3ca9b4d040
Arthur <arthurkuhn@outlook.com>
2018-02-26 11:54:01 -0500

    [Feature] Loading directly from file

--- matlab/Package/loadFromFile.m
+++ matlab/Package/loadFromFile.m
@@ -1,4 +1,4 @@
-function [ sig, fs ] = loadFromFile( filePath )
+function [ sig, fs ] = loadFromFile( file )
 %LOADFROMFILE Loads the ECG signal from the full APEX recording file
 %   Input:
 %       filePath can be absolute or relative to working directory
@@ -13,6 +13,25 @@ function [ sig, fs ] = loadFromFile( filePath )
 %   1. The signal is first formatted in a row vector.
 %   2. The first minute of the signal is processed using our full 
 %      pipeline. If necessary, the signal is reversed (see invertIfNeeded).
+load(file + ".mat");
+
+data = eval(file);
+
+if(string(data.channels(1)) ~= 'ECG')
+    print "Unexpected data format";
+    return;
+end
+% Get the first channel (ecg)
+sig = data.data(:,1);
+if(~isrow(sig))
+    sig = sig.';
+end
+
+fs = data.Fs;
+windowInSeconds = 60;
+sig = invertIfNecessary( sig, fs, windowInSeconds);
+
+
 
 
 end
