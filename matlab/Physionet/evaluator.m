e7287f6de4e35f921b5246fa9605b82fe3342fc3
Arthur <arthurkuhn@outlook.com>
2018-02-26 13:28:47 -0500

    Evaluator

--- matlab/Physionet/evaluator.m
+++ matlab/Physionet/evaluator.m
@@ -1,1 +1,13 @@
-res = rdsamp('picsdb/infant2_ecg')
+
+% The record of interest
+record = 'picsdb/infant1_ecg';
+
+% Read the signal data
+[signal,Fs,tm]=rdsamp(record);
+
+% Read the annotations
+[ann]=rdann(record,'qrsc');
+
+% Plot
+plot(tm,signal(:,1));hold on;grid on
+plot(tm(ann),signal(ann,1),'ro','MarkerSize',4);
