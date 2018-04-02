ad2ea9b590c18f3525dbdece54649e5288e5ed33
Kumbidou <claradionet@gmail.com>
2018-04-02 12:07:48 -0400

    updates

--- completeToolbox/Pipeline/metrics.m
+++ completeToolbox/Pipeline/metrics.m
@@ -3,10 +3,10 @@
 SDNN = std(result.tachogram);
 
 %SDANN
-num5min = zeros(1:floor(length(sig)/(fs*60*5))); %floor
-for i=1:(length(sig)/(fs*60*5))
-    % pas reussi
-    num5min(i) = mean(Rlocs(i:i+1) %segment + tacho
+% num5min = zeros(1:floor(length(sig)/(fs*60*5))); %floor
+% for i=1:(length(sig)/(fs*60*5))
+%     % pas reussi
+%     num5min(i) = mean(Rlocs(i:i+1) %segment + tacho
     %lower bound = i+1*nombres de samples dans ton segment 
     % rlocs array/tacho 
     
@@ -19,7 +19,7 @@ SDSD = std(diff(result.tachogram));
 
 % NN50:
 NN50 = 0;
-for j = 2:length(result.tachogram)-1
+for j = 1:length(result.tachogram)-1
     if (abs(result.tachogram(j) - result.tachogram(j+1)) > 50)
         NN50 = NN50+1;
     end
